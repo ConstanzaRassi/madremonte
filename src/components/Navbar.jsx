61ab0e1e35d3eaaeea786e1a3a31eec27aa05ed0
ConstanzaRassi <cotirassi@gmail.com>
2023-09-04 14:20:20 -0300

    NavBar component working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,51 @@
-import React from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { useRef } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Biography from "../pages/Biography";
+import Music from "../pages/Music";
+import Nguba from "../pages/Nguba";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-function Navbar() {
-  const navRef = useRef();
-
-  const showNavbar = () => {
-    navRef.current.classList.toggle("responsive_nav");
-  };
-
+function NavbarMenu() {
   return (
-    <header>
-      <h3>Logo</h3>
-      <nav ref={navRef}>
-        <a href="/#">Home</a>
-        <a href="/#">Parte 1</a>
-        <a href="/#">Parte 2</a>
-        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-          <FaTimes />
-        </button>
-      </nav>
-      <button className="nav-btn" onClick={showNavbar}>
-        <FaBars />
-      </button>
-    </header>
+    <Router>
+      <Navbar
+        bg="dark"
+        variant="dark"
+        sticky="top"
+        expand="sm"
+        collapseOnSelect
+      >
+        <Navbar.Brand>
+          {/* <img src="{img}" /> */}
+          Logo
+        </Navbar.Brand>
+        <Navbar.Toggle className="coloring" />
+        <Navbar.Collapse>
+          <Nav>
+            <NavDropdown title="Conocimientos">
+              <NavDropdown.Item href="#">Algunas</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/nguba">
+                Nguba
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link as={Link} to="/biography">
+              Biography
+            </Nav.Link>
+            <Nav.Link as={Link} to="/music">
+              Music
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Routes>
+        <Route path="/biography" element={<Biography />}></Route>
+        <Route path="/music" element={<Music />}></Route>
+        <Route path="/nguba" element={<Nguba />}></Route>
+      </Routes>
+    </Router>
   );
 }
-export default Navbar;
+
+export default NavbarMenu;
